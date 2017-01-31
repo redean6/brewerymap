a831dbe306815b44ab57da9aeb22b3966ec89ca3
Richard Dean <redean6@gmail.com>
2017-01-31 05:23:01 -0700

    added all needed files to get map working.

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+
+ var map;
+
+ function initMap() {
+        
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.3951599802915 , lng:-105.053384019708 },
+          zoom: 15
+        });
+
+  }
