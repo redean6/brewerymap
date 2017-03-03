df16fa3a53f85c7f8f30be620cff02b8b3a47a16
Richard Dean <redean6@gmail.com>
2017-03-03 05:34:48 -0700

    View Model API

--- javascript/main.js
+++ javascript/main.js
@@ -5,38 +5,126 @@ var markers = [];
 
 
 var allBreweries = {
-  breweries: ko.observableArray([
-      {name:"Verboten Brewing & Barrel Project", location:{lat:40.396818, lng:-105.075115}, city:'Loveland'},
-      {name:"Grimm Brothers Brewhouse", location:{lat:40.396763 , lng:-105.046949}, city: 'Loveland'},
-      {name:"Loveland Ale Works", location:{lat:40.395281 , lng:-105.077335}, city: 'Loveland'},
-      {name:"City Star Brewing", location:{lat:40.305052 , lng:-105.078952}, city: 'Berthoud'},
-      {name:"Left Hand Brewing", location:{lat:40.158286 , lng:-105.115023}, city: 'Longmont'},
-      {name:"High Hops Brewing", location:{lat:40.480687 , lng:-104.935613}, city: 'Windsor'},
-      {name:"New Belgium Brewing", location:{lat:40.592796 , lng:-105.068601}, city: 'Fort Collins'},
-      {name:"Odell Brewing", location:{lat:40.589467 , lng:-105.063182}, city: 'Fort Collins'},
-      {name:"Horse & Dragon Brewing", location:{lat:40.589644 , lng:-105.045623}, city: 'Fort Collins'},
-      {name:"Zwei Brothers Brewing", location:{lat:40.522753 , lng:-105.078360}, city: 'Fort Collins'},
-      {name:"Avery Brewing", location:{lat:40.062563 , lng:-105.204743}, city: 'Boulder'}
-      //{name:"Mean Dean Brewing", location:{lat:40.3951599 , lng:-105.0533840}, city: 'Loveland'}
-    ])
+  breweries: ko.observableArray([{
+      name: "Verboten Brewing & Barrel Project",
+      location: {
+        lat: 40.396818,
+        lng: -105.075115
+      },
+      city: 'Loveland'
+    },
+    {
+      name: "Grimm Brothers Brewhouse",
+      location: {
+        lat: 40.396763,
+        lng: -105.046949
+      },
+      city: 'Loveland'
+    },
+    {
+      name: "Loveland Ale Works",
+      location: {
+        lat: 40.395281,
+        lng: -105.077335
+      },
+      city: 'Loveland'
+    },
+    {
+      name: "City Star Brewing",
+      location: {
+        lat: 40.305052,
+        lng: -105.078952
+      },
+      city: 'Berthoud'
+    },
+    {
+      name: "Left Hand Brewing",
+      location: {
+        lat: 40.158286,
+        lng: -105.115023
+      },
+      city: 'Longmont'
+    },
+    {
+      name: "High Hops Brewing",
+      location: {
+        lat: 40.480687,
+        lng: -104.935613
+      },
+      city: 'Windsor'
+    },
+    {
+      name: "New Belgium Brewing",
+      location: {
+        lat: 40.592796,
+        lng: -105.068601
+      },
+      city: 'Fort Collins'
+    },
+    {
+      name: "Odell Brewing",
+      location: {
+        lat: 40.589467,
+        lng: -105.063182
+      },
+      city: 'Fort Collins'
+    },
+    {
+      name: "Horse & Dragon Brewing",
+      location: {
+        lat: 40.589644,
+        lng: -105.045623
+      },
+      city: 'Fort Collins'
+    },
+    {
+      name: "Zwei Brothers Brewing",
+      location: {
+        lat: 40.522753,
+        lng: -105.078360
+      },
+      city: 'Fort Collins'
+    },
+    {
+      name: "Avery Brewing",
+      location: {
+        lat: 40.062563,
+        lng: -105.204743
+      },
+      city: 'Boulder'
+    }
+    //{name:"Mean Dean Brewing", location:{lat:40.3951599 , lng:-105.0533840}, city: 'Loveland'}
+  ])
 };
 
 allBreweries.query = ko.observable('');
 
 allBreweries.filteredBreweries = ko.observableArray([]);
 
-allBreweries.search = ko.computed(function(){
+allBreweries.infoWindowContent = ko.observable('');
+
+allBreweries.activeMarker = null;
+allBreweries.infoWindow = null;
+
+allBreweries.infoWindowContentChanged = ko.computed(function() {
+  console.log(allBreweries.infoWindowContent());
+  if (allBreweries.activeMarker) {
+    populateInfoWindow(allBreweries.activeMarker, allBreweries.infoWindow, allBreweries.infoWindowContent());
+  }
+});
+
+allBreweries.search = ko.computed(function() {
   //console.log(allBreweries.query());
   var query = allBreweries.query().toLowerCase();
   allBreweries.filteredBreweries([]);
   //console.log(query);
-  allBreweries.breweries().forEach(function(brewery){
+  allBreweries.breweries().forEach(function(brewery) {
     //console.log(brewery);
     //console.log(brewery.name.indexOf(query));
     var city = brewery.city.toLowerCase();
     var match = city.indexOf(query);
     //console.log(name, query, match);
-    if (match> -1) {
+    if (match > -1) {
       allBreweries.filteredBreweries.push(brewery);
     }
 
@@ -46,13 +134,16 @@ allBreweries.search = ko.computed(function(){
 
 
 function initMap() {
-        
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.3951599802915 , lng:-105.053384019708 },
-          zoom: 15
-        });
 
-  	var bounds = new google.maps.LatLngBounds();
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 40.3951599802915,
+      lng: -105.053384019708
+    },
+    zoom: 15
+  });
+
+  var bounds = new google.maps.LatLngBounds();
 
 
 
@@ -60,7 +151,7 @@ function initMap() {
 
 
 
-  document.getElementById('zoom').addEventListener('click',showMarkers);
+  document.getElementById('zoom').addEventListener('click', showMarkers);
 
   ko.applyBindings(allBreweries);
 }
@@ -69,34 +160,36 @@ function initMap() {
 
 
 
-function showMarkers(){
+function showMarkers() {
 
-    function setMapOnAll(map) {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-        }
-      }
+  function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
 
-      // Removes the markers from the map, but keeps them in the array.
-    function clearMarkers() {
-        setMapOnAll(null);
-      }
+  // Removes the markers from the map, but keeps them in the array.
+  function clearMarkers() {
+    setMapOnAll(null);
+  }
 
-    function deleteMarkers() {
-        clearMarkers();
-        markers = [];
-      }
+  function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+  }
 
-    deleteMarkers();
+  deleteMarkers();
 
-    var infoWindow = new google.maps.InfoWindow()
+  var infoWindow = new google.maps.InfoWindow();
 
-    var hereNow
+  allBreweries.infoWindow = infoWindow;
+
+  var hereNow
 
 
 
   var bounds = new google.maps.LatLngBounds();
-  for (var i = 0; i < allBreweries.filteredBreweries().length; i++){
+  for (var i = 0; i < allBreweries.filteredBreweries().length; i++) {
     var position = allBreweries.filteredBreweries()[i].location;
     var breweryCity = allBreweries.filteredBreweries()[i].city;
     var breweryName = allBreweries.filteredBreweries()[i].name;
@@ -106,7 +199,7 @@ function showMarkers(){
       map: map,
       position: position,
       id: i,
-      icon:'images/bar.png',
+      icon: 'images/bar.png',
       title: breweryName,
       city: breweryCity,
       lat: brewerylat,
@@ -114,110 +207,116 @@ function showMarkers(){
 
     });
 
-function apiData(marker) {
-  console.log(marker.lat, marker.lng);
-  //var fourSquareAPI = 'https://api.foursquare.com/v2/venues/search?ll=' + marker.lat + ',' + marker.lng + '&oatuh_token=NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP&v=201';
-  //var fourSquareHours= 'https://api.foursquare.com/v2/venues/'+ fourSquareAPI.response.venues.id +'/hours?oauth_token=NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP&v=20170223'
-  // $.getJSON(fourSquareAPI, function(data){
-  //     console.log(data.response.venues.id);
-  // })
+    function apiData(marker) {
+      // console.log(marker.lat, marker.lng);
+      //var fourSquareAPI = 'https://api.foursquare.com/v2/venues/search?ll=' + marker.lat + ',' + marker.lng + '&oatuh_token=NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP&v=201';
+      //var fourSquareHours= 'https://api.foursquare.com/v2/venues/'+ fourSquareAPI.response.venues.id +'/hours?oauth_token=NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP&v=20170223'
+      // $.getJSON(fourSquareAPI, function(data){
+      //     console.log(data.response.venues.id);
+      // })
 
-  var CLIENT_ID = 'NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP',
-    CLIENT_SECRET = 'GODNFJRZ4M1BZCS3SVV1VIUTMWE3XFYRATBEV2NX1XNEW1UF',
-    version = '20130815',
-    city = 'San Francisco',
-    query = marker.title,
-    base_url = "https://api.foursquare.com/v2/venues",
-    lat_lng = marker.lat +','+ marker.lng;
+      var CLIENT_ID = 'NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP',
+        CLIENT_SECRET = 'GODNFJRZ4M1BZCS3SVV1VIUTMWE3XFYRATBEV2NX1XNEW1UF',
+        version = '20130815',
+        city = 'San Francisco',
+        query = marker.title,
+        base_url = "https://api.foursquare.com/v2/venues",
+        lat_lng = marker.lat + ',' + marker.lng;
 
-   console.log(lat_lng);
+      //console.log(lat_lng);
 
-  $.ajax({
-    url: base_url + '/search',
-    dataType: 'json',
-    data: {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      ll: lat_lng,
-      v: version,
-      query: query,
-      async: true
-    }
-  }).done(function(result) {
-      console.log(result);
-      //console.log(result.response.venues[0].id);
-      var id = result.response.venues[0].id;
-      var hereNow = result.response.venues[0].hereNow.count;
-      //console.log(hereNow);
-      getDetails(id);
-    }).fail(function(error) {
-      console.log(error);
-    });
-  }
-
-  function getDetails(id) {
-  	console.log(id);
-  	var CLIENT_ID = 'NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP',
-    	CLIENT_SECRET = 'GODNFJRZ4M1BZCS3SVV1VIUTMWE3XFYRATBEV2NX1XNEW1UF',
-    	version = '20130815',
-    	hours_url = "https://api.foursquare.com/v2/venues/"+id+"/hours",
-    	lat_lng = marker.lat +','+ marker.lng;
-
-    $.ajax({
-    url: hours_url,
-    dataType: 'json',
-    data: {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      v: version
-    }
-      
-    }).done(function(hourResult) {
-    console.log(hourResult);
-
-    //console.log(result.response.hours.timeframes);
-
-    //var timeframes = result.response.hours.timeframes;
-
-    //console.log(timeframes);
-
-  }).fail(function(error) {
-    console.log(error);
-  });
-
-  //Create a function here that iterates over the timeframe array. 
-
-}
-
-
-
-      function populateInfoWindow(marker, infowindow) {
-      	
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div >' + marker.title + '</div>' + '<div>'+ marker.lat + '</div>'+ '<div>'+ marker.lng + '</div>'+ '<div id="hours"></div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-          });
+      $.ajax({
+        url: base_url + '/search',
+        dataType: 'json',
+        data: {
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          ll: lat_lng,
+          v: version,
+          query: query,
+          async: true
         }
-      }
+      }).done(function(result) {
+        console.log(result);
+        //console.log(result.response.venues[0].id);
+        var id = result.response.venues[0].id;
+        var hereNow = result.response.venues[0].hereNow.count;
+        //console.log(hereNow);
+        allBreweries.infoWindowContent('<p>People here right now: ' + hereNow + '</p>' );
+        //var test = Math.random();
+
+        //allBreweries.infoWindowContent('<p>People here right now: ' + test + '</p>');
+      
+        //getDetails(id);
+      }).fail(function(error) {
+        console.log(error);
+      });
+    }
+
+    // function getDetails(id) {
+    //   //console.log(id);
+    //   var CLIENT_ID = 'NCZR4E52CTOVIW2C0HMOZLPZL3ZLOUQSUVNB55MUYBQNEVJP',
+    //     CLIENT_SECRET = 'GODNFJRZ4M1BZCS3SVV1VIUTMWE3XFYRATBEV2NX1XNEW1UF',
+    //     version = '20130815',
+    //     hours_url = "https://api.foursquare.com/v2/venues/" + id + "/hours",
+    //     lat_lng = marker.lat + ',' + marker.lng;
+
+    //   $.ajax({
+    //     url: hours_url,
+    //     dataType: 'json',
+    //     data: {
+    //       client_id: CLIENT_ID,
+    //       client_secret: CLIENT_SECRET,
+    //       v: version
+    //     }
+
+    //   }).done(function(hourResult) {
+        //console.log("hourResult");
+
+        var currentContent = allBreweries.infoWindowContent();
+        //var openingHours = "tbd";
+
+        //allBreweries.infoWindowContent(currentContent + '<h6>Opening Hours</h6><p>' +  + '</p>');
+
+        // console.log(hourResult);
+
+        //console.log(result.response.hours.timeframes);
+
+        //var timeframes = result.response.hours.timeframes;
+
+        //console.log(timeframes);
+
+      //Create a function here that iterates over the timeframe array. 
+
 
     markers.push(marker);
 
     marker.addListener('click', function() {
-            populateInfoWindow(this, infoWindow);
-            apiData(this);
-          });
+      //populateInfoWindow(this, infoWindow);
+      allBreweries.activeMarker = this;
+      apiData(this);
+    });
 
     bounds.extend(marker.position);
 
-    
-  };//closes for loop
 
-   map.fitBounds(bounds);
+  }; //closes for loop
 
-  };//closes show markers
+  map.fitBounds(bounds);
 
+}; //closes show markers
+
+function populateInfoWindow(marker, infoWindow, currentContent) {
+
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infoWindow.marker != marker) {
+    infoWindow.marker = marker;
+    infoWindow.setContent('<div >' + marker.title + '</div>' + '<div>' + marker.lat + '</div>' + '<div>' + marker.lng + '</div>' + '<div id="here"> ' + currentContent + '</div>');
+    //infoWindow.setContent(currentContent);
+    infoWindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infoWindow.addListener('closeclick', function() {
+      infoWindow.marker = null;
+    });
+  }
+}
